22a0a183932af6c10330ec65c8051e285de70722
zqxssl214 <andrejcubaka1822@gmail.com>
2025-11-26 19:55:12 +0700

    Add main in sort.c

--- sort.c
+++ sort.c
@@ -15,4 +15,19 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
+
+int main() {
+    int a[] = {4, 2, 0, 8, 5};
+    int n = sizeof(a) / sizeof(a[0]);
+    
+    bubbleSort(a, n);
+    
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
 // This is end of file
